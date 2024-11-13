fbbab6856e40024aa39c9b188a2d7d6be84cdd85
Pedro Antonio <1919952@alu.murciaeduca.es>
2024-11-13 17:07:01 +0100

    Ejercicio4

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -20,8 +20,6 @@
                             <textarea name="summary" id="summary" rows="3"
                                       class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">{{ old('summary') }}</textarea>
                         </div>
-
-
                         <div>
                             <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
                             <select name="status" id="status"
